5014626f8542589e55ceaed5ead6f4b18a9707ef
GTRows <aciroglu.fatih@gmail.com>
2023-11-05 15:44:01 +0300

    Add animated transition to splash screen after a delay

--- SmartLabEntry/View/SplashView.swift
+++ SmartLabEntry/View/SplashView.swift
@@ -8,32 +8,43 @@
 import SwiftUI
 
 struct SplashView: View {
+    @State private var isActive = false
     var body: some View {
-        VStack {
-            Spacer()
-            Divider()
-            Image("Logo")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 200, height: 200)
-                .padding(.bottom,20)
-            
-            Text("Yapay Zeka ve\nVeri Bilimi  Laboratuvarı")
-                .font(.custom("Comfortaa", size: 25))
-                .fontWeight(.bold)
-                .foregroundColor(Color("LightColor"))
-                .multilineTextAlignment(.center)
-                .padding(.bottom, 20)
-            
-            Spacer()
+        if isActive {
+            AuthView()
+        } else {
+            VStack {
+                Spacer()
+                Divider()
+                Image("Logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200)
+                    .padding(.bottom, 20)
+
+                Text("Yapay Zeka ve\nVeri Bilimi  Laboratuvarı")
+                    .font(.custom("Comfortaa", size: 25))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color("LightColor"))
+                    .multilineTextAlignment(.center)
+                    .padding(.bottom, 20)
+
+                Spacer()
+            }
+            .background(
+                LinearGradient(gradient: Gradient(colors: [Color("Blue"), Color("DarkBlue")]), startPoint: .top, endPoint: .bottom)
+                    .edgesIgnoringSafeArea(.all)
+            )
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                    withAnimation {
+                        isActive = true
+                    }
+                }
+            }
         }
-        .background(
-            LinearGradient(gradient: Gradient(colors: [Color("Blue"), Color("DarkBlue")]), startPoint: .top, endPoint: .bottom)
-                .edgesIgnoringSafeArea(.all)
-        )
     }
 }
-
 
 #Preview {
     SplashView()
